837c3367ead025abe36bad10edf3a7eb5aeb00b5
austonpramodh <austonpramodh@gmail.com>
2019-02-05 14:42:30 +0530

    product routes updated, delete, update route added

--- src/Routes/product.js
+++ src/Routes/product.js
@@ -1,22 +1,42 @@
 const router = require("express").Router();
 const ProductDbFunctions = require("../utils/ProductDBFunctions");
 const Tokens = require("../utils/tokens");
+const errCodes = require("../Constants/errCodes");
 
-router.post("/addproduct", (req, res) => {
+router.post("/add", (req, res) => {
   const userEmail = Tokens.extractUserEmail(Tokens.extractTokenRequest(req));
-  const { product } = req.body;
-  product.seller = userEmail;
-  ProductDbFunctions.addProduct(product, (err) => {
+  const { body } = req;
+  body.seller = userEmail;
+  ProductDbFunctions.add(body, (err) => {
     if (err) {
       res.json({ err: "adding product" });
     } else res.json({ success: true });
   });
 });
 
-router.get("/getProducts", (req, res) => {
+router.get("/get", (req, res) => {
   const userEmail = Tokens.extractUserEmail(Tokens.extractTokenRequest(req));
-  ProductDbFunctions.getProducts(userEmail, (err, data) => {
+  ProductDbFunctions.getAll(userEmail, (err, data) => {
     res.json({ data });
+  });
+});
+
+// delete products --------------
+router.post("/delete", (req, res) => {
+  const { _id } = req.body;
+  ProductDbFunctions.delete(_id, (err) => {
+    if (err) {
+      res.json({ errCode: errCodes.ERROR_DELETING_PRODUCT });
+    } else res.json({ success: true });
+  });
+});
+
+// update product -----------
+router.post("/update", (req, res) => {
+  ProductDbFunctions.update(req.body, (err) => {
+    if (err) {
+      res.json({ err });
+    } else res.json({ success: true });
   });
 });
 
--- src/utils/ProductDBFunctions.js
+++ src/utils/ProductDBFunctions.js
@@ -1,12 +1,20 @@
 const ProductSchema = require("../models/Product");
 
-module.exports.addProduct = (product, cb) => {
+module.exports.add = (product, cb) => {
   const newProduct = new ProductSchema({
     ...product,
   });
   newProduct.save(cb);
 };
 
-module.exports.getProducts = (userEmail, cb) => {
+module.exports.getAll = (userEmail, cb) => {
   ProductSchema.find({ seller: userEmail }, cb);
+};
+
+module.exports.delete = (_id, cb) => {
+  ProductSchema.deleteOne({ _id }, cb);
+};
+
+module.exports.update = (product, cb) => {
+  ProductSchema.updateOne({ _id: product._id }, product, cb);
 };
