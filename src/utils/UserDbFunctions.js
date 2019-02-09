d6c0b14c7e3c2e5f877d96994106de2233deaa8e
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-02-09 13:28:40 +0530

    disbaled bcrypt

--- src/utils/UserDbFunctions.js
+++ src/utils/UserDbFunctions.js
@@ -1,30 +1,30 @@
-const bcrypt = require("bcrypt");
-const uuidv1 = require("uuid/v1");
+// const bcrypt = require("bcrypt");
+// const uuidv1 = require("uuid/v1");
 const UserSchema = require("../models/User");
-const encryptionConstants = require("../Constants/encryption");
+// const encryptionConstants = require("../Constants/encryption");
 
 module.exports.createUser = (user, cb) => {
   const newUser = new UserSchema({
     ...user,
   });
-  bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
-    bcrypt.hash(newUser.password, salt, (errhash, hash) => {
-      newUser.password = hash;
-      newUser.uuid = uuidv1();
-      newUser.save(cb);
-    });
-  });
+  // bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
+  // bcrypt.hash(newUser.password, salt, (errhash, hash) => {
+  // newUser.password = hash;
+  // newUser.uuid = uuidv1();
+  newUser.save(cb);
+  // });
+  // });
 };
 
 module.exports.updatePassword = (user, newPassword, cb) => {
-  bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
-    bcrypt.hash(newPassword, salt, (errhash, hash) => {
-      user.uuid = uuidv1();
-      user.password = hash;
-      user.otp = "";
-      UserSchema.updateOne({ email: user.email }, user, cb);
-    });
-  });
+  // bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
+  // bcrypt.hash(newPassword, salt, (errhash, hash) => {
+  // user.uuid = uuidv1();
+  // user.password = hash;
+  user.otp = "";
+  UserSchema.updateOne({ email: user.email }, user, cb);
+  // });
+  // });
 };
 
 module.exports.findUser = ({ email }, cb) => {
@@ -32,7 +32,7 @@ module.exports.findUser = ({ email }, cb) => {
 };
 
 module.exports.comparePassword = ({ password, dbPassword }, cb) => {
-  bcrypt.compare(password, dbPassword, cb);
+  // bcrypt.compare(password, dbPassword, cb);
 };
 
 // update User
