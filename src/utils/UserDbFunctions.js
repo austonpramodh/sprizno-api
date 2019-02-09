a234bf1eea2cbf738d25e95b9dd7f71157b22671
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-02-09 18:21:22 +0530

    argon2 replaced by bcrypt

--- src/utils/UserDbFunctions.js
+++ src/utils/UserDbFunctions.js
@@ -1,33 +1,33 @@
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const uuidv1 = require("uuid/v1");
-const argon2 = require("argon2");
+// const argon2 = require("argon2");
 const UserSchema = require("../models/User");
-// const encryptionConstants = require("../Constants/encryption");
+const encryptionConstants = require("../Constants/encryption");
 
 module.exports.createUser = async (user, cb) => {
   const newUser = new UserSchema({
     ...user,
   });
-  const hash = await argon2.hash(newUser.password);
-  newUser.password = hash;
-  // bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
-  // bcrypt.hash(newUser.password, salt, (errhash, hash) => {
+  // const hash = await argon2.hash(newUser.password);
   // newUser.password = hash;
-  newUser.uuid = uuidv1();
-  newUser.save(cb);
-  // });
-  // });
+  bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
+    bcrypt.hash(newUser.password, salt, (errhash, hash) => {
+      newUser.password = hash;
+      newUser.uuid = uuidv1();
+      newUser.save(cb);
+    });
+  });
 };
 
 module.exports.updatePassword = (user, newPassword, cb) => {
-  // bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
-  // bcrypt.hash(newPassword, salt, (errhash, hash) => {
-  // user.uuid = uuidv1();
-  // user.password = hash;
-  user.otp = "";
-  UserSchema.updateOne({ email: user.email }, user, cb);
-  // });
-  // });
+  bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
+    bcrypt.hash(newPassword, salt, (errhash, hash) => {
+      user.uuid = uuidv1();
+      user.password = hash;
+      user.otp = "";
+      UserSchema.updateOne({ email: user.email }, user, cb);
+    });
+  });
 };
 
 module.exports.findUser = ({ email }, cb) => {
@@ -35,7 +35,7 @@ module.exports.findUser = ({ email }, cb) => {
 };
 
 module.exports.comparePassword = ({ password, dbPassword }, cb) => {
-  // bcrypt.compare(password, dbPassword, cb);
+  bcrypt.compare(password, dbPassword, cb);
 };
 
 // update User
