8107b5652041f1be26d24cd7091ea42abfc2135a
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-02-10 11:06:57 +0530

    bcrypt replaced with crypto

--- src/utils/UserDbFunctions.js
+++ src/utils/UserDbFunctions.js
@@ -1,32 +1,68 @@
-const bcrypt = require("bcrypt");
 const uuidv1 = require("uuid/v1");
-// const argon2 = require("argon2");
+const crypto = require("crypto");
 const UserSchema = require("../models/User");
-const encryptionConstants = require("../Constants/encryption");
+const cryptoConfig = require("../Constants/cryptoConfig");
 
-module.exports.createUser = async (user, cb) => {
+module.exports.createUser = async (user, callback) => {
   const newUser = new UserSchema({
     ...user,
   });
-  // const hash = await argon2.hash(newUser.password);
-  // newUser.password = hash;
-  bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
-    bcrypt.hash(newUser.password, salt, (errhash, hash) => {
-      newUser.password = hash;
-      newUser.uuid = uuidv1();
-      newUser.save(cb);
-    });
+  crypto.randomBytes(cryptoConfig.saltBytes, (err, salt) => {
+    salt = salt.toString("hex");
+    if (err) {
+      return callback(err);
+    }
+    crypto.pbkdf2(
+      newUser.password,
+      salt,
+      cryptoConfig.iterations,
+      cryptoConfig.hashBytes,
+      null,
+      (err1, hash) => {
+        if (err1) return callback(err1);
+        hash = hash.toString("hex");
+        const combined = [salt, hash].join("$");
+        newUser.uuid = uuidv1();
+        newUser.password = combined;
+        newUser.save(callback);
+        return null;
+      },
+    );
+    return null;
   });
 };
 
-module.exports.updatePassword = (user, newPassword, cb) => {
-  bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
-    bcrypt.hash(newPassword, salt, (errhash, hash) => {
-      user.uuid = uuidv1();
-      user.password = hash;
-      user.otp = "";
-      UserSchema.updateOne({ email: user.email }, user, cb);
-    });
+module.exports.updatePassword = (user, newPassword, callback) => {
+  // bcrypt.genSalt(encryptionConstants.salt, (errsalt, salt) => {
+  //   bcrypt.hash(newPassword, salt, (errhash, hash) => {
+  //     user.uuid = uuidv1();
+  //     user.password = hash;
+  //     user.otp = "";
+  //     UserSchema.updateOne({ email: user.email }, user, cb);
+  //   });
+  // });
+  crypto.randomBytes(cryptoConfig.saltBytes, (err, salt) => {
+    salt = salt.toString("hex");
+    if (err) {
+      return callback(err);
+    }
+    crypto.pbkdf2(
+      newPassword,
+      salt,
+      cryptoConfig.iterations,
+      cryptoConfig.hashBytes,
+      null,
+      (err1, hash) => {
+        if (err1) return callback(err1);
+        hash = hash.toString("hex");
+        const combined = [salt, hash].join("$");
+        user.uuid = uuidv1();
+        user.password = combined;
+        user.save(callback);
+        return null;
+      },
+    );
+    return null;
   });
 };
 
@@ -34,8 +70,23 @@ module.exports.findUser = ({ email }, cb) => {
   UserSchema.findOne({ email }, cb);
 };
 
-module.exports.comparePassword = ({ password, dbPassword }, cb) => {
-  bcrypt.compare(password, dbPassword, cb);
+module.exports.comparePassword = ({ password, dbPassword }, callback) => {
+  // get salt out
+  const salt = dbPassword.split("$")[0];
+  // get out he hash
+  const dbHash = dbPassword.split("$")[1];
+  crypto.pbkdf2(
+    password,
+    salt,
+    cryptoConfig.iterations,
+    cryptoConfig.hashBytes,
+    null,
+    (err, hash) => {
+      if (err) return callback(err, false);
+      callback(null, hash.toString("hex") === dbHash);
+      return null;
+    },
+  );
 };
 
 // update User
