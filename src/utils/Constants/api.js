a11dc1c472e0810fbc451400bed8cfbbcbddfecd
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-02-18 00:21:50 +0530

    messed with apiconstants

--- src/utils/Constants/api.js
+++ src/utils/Constants/api.js
@@ -1,9 +1,9 @@
 const api = {
   db: {
     // url: "mongodb://austonpramodh:Clickass1@ds040948.mlab.com:40948/rollswitch",
-    url: "mongodb://localhost:27017/ecommerce",
-    // url:
-    // "mongodb+srv://sprizno:sprizno123@cluster0-alayz.azure.mongodb.net/sprizno?retryWrites=true",
+    // url: "mongodb://localhost:27017/ecommerce",
+    url:
+      "mongodb+srv://sprizno:sprizno123@cluster0-alayz.azure.mongodb.net/sprizno?retryWrites=true",
   },
 };
 
