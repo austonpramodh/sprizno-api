83421128aac5034a3c45989c908a78fb06332ea3
Auston Pramodh Barboza <austonpramodh@gmail.com>
2019-03-20 01:25:12 +0530

    status route added

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -3,6 +3,8 @@ const sellerRoutes = require("./seller/index");
 const userRoutes = require("./user/index");
 const adminRoutes = require("./admin/index");
 const commonRoutes = require("./common/index");
+const errCodes = require("../utils/Constants/errCodes");
+const api = require("../utils/Constants/api");
 
 const router = express.Router();
 
@@ -10,5 +12,17 @@ router.use("/seller", sellerRoutes);
 router.use("/user", userRoutes);
 router.use("/admin", adminRoutes);
 router.use("/common", commonRoutes);
+
+router.get("/status", (req, res) => {
+  res.status(200).json({
+    environment: process.env.NODE_ENV ? process.env.NODE_ENV : "development or unknown",
+    message: "all fine",
+    dbUrl: api.DB.URL,
+  });
+});
+
+router.use((req, res) => {
+  res.status(404).json({ errCode: errCodes.ROUTE_NOT_FOUND, errMessage: "Unknown Route" });
+});
 
 module.exports = router;
--- src/utils/middlewares/clientValidate.js
+++ src/utils/middlewares/clientValidate.js
@@ -2,7 +2,7 @@ const errCodes = require("../Constants/errCodes");
 
 const clientValidate = (request, response, next) => {
   const { client } = request.headers;
-  if (client && client === "web") {
+  if ((client && client === "web") || request.path === "/status") {
     next();
   } else response.status(403).json({ errCode: errCodes.INVALID_CLIENT, errMessage: "invalid client" });
 };
